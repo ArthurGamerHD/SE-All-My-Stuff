39ccb08d0a7fe86470d032ca27bbafd202562d6f
Brian Ronald <brian@ppcis.org>
2022-05-15 14:37:40 +0000

    Add project files.

---
+++ All My Stuff/ManagedDisplay.cs
@@ -0,0 +1,191 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+using System;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+
+        class ManagedDisplay
+        {
+            private IMyTextSurface surface;
+            private RectangleF viewport;
+            private MySpriteDrawFrame frame;
+            private float StartHeight = 0f;
+            private float HeadingHeight = 35f;
+            private float LineHeight = 30f;
+            private float HeadingFontSize = 1.3f;
+            private float RegularFontSize = 1.0f;
+            private Vector2 Position;
+            private int WindowSize;         // Number of lines shown on screen at once after heading
+            private Color HighlightColor;
+            private int linesToSkip;
+            private bool monospace;
+            private readonly String SpritePrefix = "MyObjectBuilder_Component/";
+            private bool MakeSpriteCacheDirty = false;
+            private string previousType = "";
+
+            public ManagedDisplay(IMyTextSurface surface, float scale = 1.0f, Color highlightColor = new Color(), int linesToSkip = 0, bool monospace = false)
+            {
+                this.surface = surface;
+                this.HighlightColor = highlightColor;
+                this.linesToSkip = linesToSkip;
+                this.monospace = monospace;
+
+                // Scale everything!
+                StartHeight *= scale;
+                HeadingHeight *= scale;
+                LineHeight *= scale;
+                HeadingFontSize *= scale;
+                RegularFontSize *= scale;
+
+                surface.ContentType = ContentType.SCRIPT;
+                surface.Script = "TSS_FactionIcon";
+                Vector2 padding = surface.TextureSize * (surface.TextPadding / 100);
+                viewport = new RectangleF((surface.TextureSize - surface.SurfaceSize) / 2f + padding, surface.SurfaceSize - (2*padding));
+                WindowSize = ((int)((viewport.Height - 10 * scale) / LineHeight));
+            }
+
+            private void AddHeading()
+            {
+                surface.Script = "";
+                surface.ScriptBackgroundColor = Color.Black;
+                Position = new Vector2(viewport.Width / 10f, StartHeight) + viewport.Position;
+                frame.Add(new MySprite()
+                {
+                    Type = SpriteType.TEXTURE,
+                    Data = "Textures\\FactionLogo\\Builders\\BuilderIcon_1.dds",
+                    Position = Position + new Vector2(0f,LineHeight/2f),
+                    Size = new Vector2(LineHeight,LineHeight),
+                    RotationOrScale = HeadingFontSize,
+                    Color = HighlightColor,
+                    Alignment = TextAlignment.CENTER
+                });
+                Position.X += viewport.Width / 8f;
+                frame.Add(new MySprite()
+                {
+                    Type = SpriteType.TEXT,
+                    Data = "Item",
+                    Position = Position,
+                    RotationOrScale = HeadingFontSize,
+                    Color = HighlightColor,
+                    Alignment = TextAlignment.LEFT,
+                    FontId = "White"
+                });
+                Position.X += viewport.Width * 6f / 8f;
+                frame.Add(new MySprite()
+                {
+                    Type = SpriteType.TEXT,
+                    Data = "Stock",
+                    Position = Position,
+                    RotationOrScale = HeadingFontSize,
+                    Color = HighlightColor,
+                    Alignment = TextAlignment.RIGHT,
+                    FontId = "White"
+                });
+                Position.Y += HeadingHeight;
+            }
+            private void RenderRow(Program.Item item)
+            {
+                Color TextColor;
+                if (item.Amount == 0)
+                {
+                    TextColor = Color.Brown;
+                }
+                else
+                {
+                    TextColor = Color.Gray;
+                }
+                if (previousType != item.ItemType)
+                {
+                    previousType = item.ItemType;
+                    frame.Add(new MySprite()
+                    {
+                        Type = SpriteType.TEXTURE,
+                        Data = "SquareSimple",
+                        Position = new Vector2(viewport.X,Position.Y),
+                        Size = new Vector2(viewport.Width, 1),
+                        RotationOrScale = 0,
+                        Color = HighlightColor,
+                    });
+                }
+                frame.Add(new MySprite()
+                {
+                    Type = SpriteType.TEXTURE,
+                    Data = item.Sprite,
+                    Position = Position + new Vector2(0f,LineHeight/2f),
+                    Size = new Vector2(LineHeight,LineHeight),
+                    RotationOrScale = 0,
+                    Color = TextColor,
+                    Alignment = TextAlignment.CENTER,
+                });
+                Position.X += viewport.Width / 8f;
+                frame.Add(new MySprite()
+                {
+                    Type = SpriteType.TEXT,
+                    Data = item.Name,
+                    Position = Position,
+                    RotationOrScale = RegularFontSize,
+                    Color = TextColor,
+                    Alignment = TextAlignment.LEFT,
+                    FontId = monospace?"Monospace":"White"
+                });
+                Position.X += viewport.Width * 6f / 8f;
+                frame.Add(new MySprite()
+                {
+                    Type = SpriteType.TEXT,
+                    Data = item.Amount.ToString(),
+                    Position = Position,
+                    RotationOrScale = RegularFontSize,
+                    Color = TextColor,
+                    Alignment = TextAlignment.RIGHT,
+                    FontId = monospace?"Monospace":"White"
+                });
+            }
+
+            internal void Render(SortedDictionary<String, Program.Item> Stock)
+            {
+                MakeSpriteCacheDirty = !MakeSpriteCacheDirty;
+                frame = surface.DrawFrame();
+                if (MakeSpriteCacheDirty)
+                {
+                    frame.Add(new MySprite()
+                    {
+                        Type = SpriteType.TEXTURE,
+                        Data = "SquareSimple",
+                        Color = surface.BackgroundColor,
+                    });
+                }
+                AddHeading();
+                int renderLineCount = 0;
+                foreach (var item in Stock.Keys)
+                {
+                    if (++renderLineCount > linesToSkip)
+                    {
+                        Position.X = viewport.Width / 10f + viewport.Position.X;
+                        if (renderLineCount >= linesToSkip && renderLineCount < linesToSkip + WindowSize)
+                            RenderRow(Stock[item]);
+                        Position.Y += LineHeight;
+                    }
+                }
+                frame.Dispose();
+            }
+        }
+    }
+}
---
+++ All My Stuff/Program.cs
@@ -0,0 +1,247 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        List<IMyTerminalBlock> Containers = new List<IMyTerminalBlock>();
+        string Version = "Version 1.0.0";
+        MyIni ini = new MyIni();
+        string ConfigSection = "Inventory";
+        string DisplaySectionPrefix = "Display";
+        StringBuilder SectionCandidateName = new StringBuilder();
+        List<String> SectionNames = new List<string>();
+        SortedDictionary<string, Item> Stock = new SortedDictionary<string, Item>();
+        List<MyInventoryItem> Items = new List<MyInventoryItem>();
+        List<MyIniKey> iniKeys = new List<MyIniKey>();
+        List<MyProductionItem> Queue = new List<MyProductionItem>();
+        List<ManagedDisplay> Screens = new List<ManagedDisplay>();
+        IEnumerator<bool> _stateMachine;
+        int delayCounter = 0;
+        int delay;
+        bool rebuild = false;
+
+        public class Item
+        {
+            public Item(MyInventoryItem item, int Amount = 0)
+            {
+                this.Sprite = item.Type.ToString();
+                this.Name = item.Type.SubtypeId;
+                this.ItemType = item.Type.TypeId;
+                this.Amount = Amount;
+            }
+            public int Amount;
+            public String Sprite;
+            public String Name;
+            public String ItemType;
+        }
+
+        public void GetBlocks()
+        {
+            Containers.Clear();
+            Screens.Clear();
+            GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(Containers, block =>
+            {
+                if (!block.IsSameConstructAs(Me))
+                    return false;
+                TryAddDiscreteScreens(block);
+                TryAddScreen(block);
+                if (!block.HasInventory)
+                    return false;
+                return true;
+            });
+        }
+
+        void AddScreen(IMyTextSurfaceProvider provider, int displayNumber, string section)
+        {
+            var display = ((IMyTextSurfaceProvider)provider).GetSurface(displayNumber);
+            var linesToSkip = ini.Get(section, "skip").ToInt16();
+            bool monospace = ini.Get(section, "mono").ToBoolean();
+            float scale = ini.Get(section, "scale").ToSingle(1.0f);
+            string DefaultColor = "FF4500";
+            string ColorStr = ini.Get(section, "color").ToString(DefaultColor);
+            if (ColorStr.Length < 6)
+                ColorStr = DefaultColor;
+            Color color = new Color()
+            {
+                R = byte.Parse(ColorStr.Substring(0, 2), System.Globalization.NumberStyles.HexNumber),
+                G = byte.Parse(ColorStr.Substring(2, 2), System.Globalization.NumberStyles.HexNumber),
+                B = byte.Parse(ColorStr.Substring(4, 2), System.Globalization.NumberStyles.HexNumber),
+                A = 255
+            };
+            Screens.Add(new ManagedDisplay(display, scale, color, linesToSkip, monospace));
+        }
+
+        private void TryAddDiscreteScreens(IMyTerminalBlock block)
+        {
+            IMyTextSurfaceProvider Provider = block as IMyTextSurfaceProvider;
+            if (null == Provider || Provider.SurfaceCount == 0)
+                return;
+            StringComparison ignoreCase = StringComparison.InvariantCultureIgnoreCase;
+            ini.TryParse(block.CustomData);
+            ini.GetSections(SectionNames);
+            foreach (var section in SectionNames)
+            {
+                if (section.StartsWith(DisplaySectionPrefix, ignoreCase))
+                {
+                    for (int displayNumber = 0; displayNumber < Provider.SurfaceCount; ++displayNumber)
+                    {
+                        SectionCandidateName.Clear();
+                        SectionCandidateName.Append(DisplaySectionPrefix).Append(displayNumber.ToString());
+                        if (section.Equals(SectionCandidateName.ToString(), ignoreCase) && ini.Get(section, "script").ToString().Equals(ConfigSection, ignoreCase))
+                        {
+                            AddScreen(Provider, displayNumber, section);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void TryAddScreen(IMyTerminalBlock block)
+        {
+            IMyTextSurfaceProvider Provider = block as IMyTextSurfaceProvider;
+            if (null == Provider || Provider.SurfaceCount == 0 || !MyIni.HasSection(block.CustomData, ConfigSection))
+                return;
+            ini.TryParse(block.CustomData);
+            var displayNumber = ini.Get(ConfigSection, "display").ToUInt16();
+            if (displayNumber < ((IMyTextSurfaceProvider)Provider).SurfaceCount || ((IMyTextSurfaceProvider)Provider).SurfaceCount == 0)
+            {
+                AddScreen(Provider, displayNumber, ConfigSection);
+            }
+            else
+            {
+                Echo("Warning: " + block.CustomName + " doesn't have a display number " + ini.Get(ConfigSection, "display").ToString());
+            }
+        }
+
+        public void RunItemCounter()
+        {
+            if (_stateMachine != null)
+            {
+                bool hasMoreSteps = _stateMachine.MoveNext();
+
+                if (hasMoreSteps)
+                {
+                    Runtime.UpdateFrequency |= UpdateFrequency.Once;
+                }
+                else
+                {
+                    _stateMachine.Dispose();
+                    _stateMachine = null;
+                }
+            }
+        }
+
+        public IEnumerator<bool> CountItems()
+        {
+            foreach (var Item in Stock.Keys)
+                Stock[Item].Amount = 0;
+            yield return true;
+            ReadConfig();
+            yield return true;
+            yield return true;
+            foreach (var container in Containers)
+            {
+                for (int i = 0; i < container.InventoryCount; ++i)
+                {
+                    var inventory = container.GetInventory(i);
+                    if (inventory.ItemCount > 0)
+                    {
+                        Items.Clear();
+                        inventory.GetItems(Items);
+                        foreach (var item in Items)
+                        {
+                            string key = item.Type.ToString();
+                            if (!Stock.ContainsKey(key))
+                                Stock.Add(key, new Item(item));
+                            Stock[key].Amount += item.Amount.ToIntSafe();
+                        }
+                        yield return true;
+                    }
+                }
+            }
+            EchoStuff();
+        }
+
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+            GetBlocks();
+            ReadConfig();
+        }
+
+        private void ReadConfig()
+        {
+            if (ini.TryParse(Me.CustomData))
+            {
+                delay = ini.Get(ConfigSection, "delay").ToInt32(3);
+            }
+        }
+
+        private void EchoStuff()
+        {
+            Echo(Version);
+            Echo(Screens.Count + " screens");
+            Echo(Containers.Count + " blocks with inventories");
+            Echo(Stock.Count + " items being tracked");
+            foreach (var display in Screens)
+            {
+                display.Render(Stock);
+            }
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            if ((updateSource & UpdateType.Once) == UpdateType.Once)
+            {
+                RunItemCounter();
+            }
+            if ((updateSource & UpdateType.Update100) == UpdateType.Update100)
+            {
+                if (delayCounter > delay && _stateMachine == null)
+                {
+                    if (rebuild)
+                    {
+                        rebuild = false;
+                        GetBlocks();
+                        ReadConfig();
+                    }
+                    delayCounter = 0;
+                    _stateMachine = CountItems();
+                    RunItemCounter();
+                }
+                else
+                {
+                    ++delayCounter;
+                }
+            }
+            if (argument == "count" && _stateMachine == null)
+            {
+                _stateMachine = CountItems();
+                RunItemCounter();
+            }
+            if (argument == "rebuild")
+            {
+                rebuild = true;
+            }
+        }
+    }
+}
